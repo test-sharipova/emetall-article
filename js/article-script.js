22b71658da33b4705a1d77f3f763b48a665dd8c2
DinaSharipova <sharipova0308@gmail.com>
2025-07-13 23:35:04 +0500

    add main

---
+++ js/article-script.js
@@ -0,0 +1,38 @@
+$(document).ready(function() {
+  $('.article__active__item--original, .article__active__item--clone').on('click', function() {
+    $('.article__contents').toggleClass('article__contents_active');
+  });
+  
+  $('.article-close-contents').on('click', function() {
+    $('.article__contents').removeClass('article__contents_active');
+  });
+
+
+  const $original = $('.article__active__item--original');
+  const $clonePlace = $('.article__active__item--clone');
+  const $articleActive = $('.article__active');
+  const $targetHeading = $('#target-heading');
+  
+  // Создаем клон элемента
+  const $clone = $original.clone().removeClass('article__active__item--original');
+  
+  function updatePosition() {
+    const targetTop = $targetHeading.offset().top;
+    const articleBottom = $articleActive.offset().top + $articleActive.outerHeight();
+    const scrollTop = $(window).scrollTop();
+    
+    if (targetTop <= articleBottom) {
+      // Показываем клон в sticky-контейнере
+      $clonePlace.html($clone).css('display', 'block');
+      $original.hide();
+    } else {
+      // Показываем оригинал в контенте
+      $clonePlace.css('display', 'none');
+      $original.show();
+    }
+  }
+  
+  $(window).scroll(updatePosition);
+  updatePosition(); // Инициализация
+
+});
